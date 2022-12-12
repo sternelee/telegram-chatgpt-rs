f00e5a2ef382047cfcf84f97d7b0a6ca0bf9d0a9
李兴广 <lixingguang@xunlei.com>
2022-12-12 13:17:45 +0800

    feat: 增加chatgpt工具

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use teloxide::prelude::*;
+use chatgpt::prelude::*;
+
+#[tokio::main]
+async fn main() -> chatgpt::Result<()> {
+    pretty_env_logger::init();
+    log::info!("Starting throw dice bot...");
+
+    // 创建机器人
+    let bot = Bot::from_env();
+
+    // 连接 chatgpt
+    let token: String = std::env::var("SESSION_TOKEN").unwrap();
+    let mut client = ChatGPT::new(token)?;
+    client.refresh_token().await?;
+
+    let mut conversation = client.new_conversation();
+
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        let text = msg.text();
+        let response: String = conversation.send_message(&client, text).await?;
+        bot.send_message(msg.chat.id, response).await?;
+        Ok(())
+    })
+    .await;
+}
